71b5e1bd82e91c27b840e5951c6f1846896d5664
hidaya1234913 <hidaya.chatioui@etu.uae.ac.ma>
2026-05-19 10:22:15 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,15 @@
+import sys 
+print("=" * 40)
+print("bienvenue dans mon premier job jenkins")
+print("=" $ 40)
+
+nom=input("quel est ton nom?")
+print(f"Bonjour{nom}, ton job jenkins a réussi")
+
+a=10
+b=5
+print(f"{a}+{b}={a+b}")
+print(f"{a}-{b}={a-b}")
+
+assert a+b == 15,"le test a échoué"
+print("tous les tests passent avec succès")
